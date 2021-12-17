8f156ce23e1b4e01add00cd2e73b29cd38c2fcc0
Jeff Lai <pjefflai@gmail.com>
2021-12-17 08:43:57 -0800

    add first test for Vendor constructor

---
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    [TestMethod]
+    public void VendorConstructor_CreateInstancesOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
