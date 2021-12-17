b5c22581174a80d5b8d349b30812fe8c18840e39
Jeff Lai <pjefflai@gmail.com>
2021-12-17 10:22:24 -0800

    add 4th test for GetAll method

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -38,5 +38,18 @@ namespace VendorTracker.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string vendorName01 = "test vendor 1";
+      string description01 = "test description 1";
+      string vendorName02 = "test vendor 2";
+      string description02 = "test description 2";
+      Vendor newVendor1 = new Vendor(vendorName01, description01);
+      Vendor newVendor2 = new Vendor(vendorName02, description02);
+      List<Vendor> vendorList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(vendorList, result);
+    }
   }
 }
