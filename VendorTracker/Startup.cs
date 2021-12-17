80acfe9efa63d231bef8bdd40cb0b84ba1a9b8a9
Jeff Lai <pjefflai@gmail.com>
2021-12-17 15:46:34 -0800

    add basic styling to html pages

--- VendorTracker/Startup.cs
+++ VendorTracker/Startup.cs
@@ -33,6 +33,8 @@ namespace Hangman
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
 
+      app.UseStaticFiles();
+
       app.Run(async (context) =>
       {
         await context.Response.WriteAsync("Hello World!");
